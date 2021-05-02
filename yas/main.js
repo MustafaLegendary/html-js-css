54e131de1d9bbfa92f8f152b1bcc5cc2ccbd8f8d
MustafaLegendary <mustafalegendaryx@gmail.com>
2021-05-02 17:45:24 +0300

    second commit

--- yas/main.js
+++ yas/main.js
@@ -1,4 +1,4 @@
-function benimFunction() {
+function myFunction() {
     const yıl = new Date().getFullYear();
     const doğumYılı = prompt("Doğum Yılınızı Giriniz.")
     const yaş = `Yaşınız ${yıl-doğumYılı} dır.`;
